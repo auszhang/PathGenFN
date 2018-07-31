ac2ea8d1cb9ff78049a4b59df7647a57f4c7c2e8
Austin Chiang <austinchiang1@berkeley.edu>
2018-07-30 17:12:51 -0700

    Revert "Restructuring GitHub Folder Pt. 2"

--- Fortnite Path Gen/main.py
+++ Fortnite Path Gen/main.py
@@ -6,26 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')  # routing ties webpage to Python function
 def index():
-    return render_template("homepage.html")
-
+    # result_int = distance("Lonely Lodge", (21, 91))
+    return render_template("homepage.html")  # , result_int=result_int)
 
 # Two text boxes, 1 for start and the other for end
 # Some kind of type checking
 
-@app.route('/', methods=["POST"])
-def index_post():
-    text1 = request.form['text1']
-    text2 = request.form['text2']
-    processed = process_text(text1, text2)
-    add_user_input(processed)
-    gen_edges()
-    depth_first_search()
-    path_list = gen_path()
-    return format_path(path_list)
+# @app.route('/', methods='POST')
+# def index():
+#     coords = request.form['text']
+#     processed_text = text.upper();
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
 
 
