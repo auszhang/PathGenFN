41772ff8b6d1ccd329e456ae5d49486308375fe7
mtc017 <32399837+mtc017@users.noreply.github.com>
2018-08-08 02:32:25 -0700

    added dropdown menu for start location

--- Fortnite Path Gen/main.py
+++ Fortnite Path Gen/main.py
@@ -12,17 +12,20 @@ def index():
 # Two text boxes, 1 for start and the other for end
 # Some kind of type checking
 
-@app.route('/', methods=["POST"])
-def index_post():
+@app.route('/', methods=['POST'])
+def magic():
+    start = request.form['start']
     text1 = request.form['text1']
-    text2 = request.form['text2']
-    processed = process_text(text1, text2)
+    processed = process_text(start, text1)
     add_user_input(processed)
-    # gen_edges()
     depth_first_search()
     path_list = gen_path()
     return format_path(path_list)
-
+#
+# @app.route('/', methods=['GET'])
+# def dropdown():
+#     POIs = POI_dict
+#     return render_template('homepage.html', POIs=POIs)
 
 if __name__ == '__main__':
     app.debug = True
